44c13b45e77c64b9f0866b288f00b5b95677b01a
subodhr <subodh.reddy@gmail.com>
2024-10-19 21:28:41 -0700

    Added classes for driving the bot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/el11pkg/el11DriveBot.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.el11pkg;
+
+//
+// @Copyright : All rights reserved. Belongs to FTC Team 25610
+//
+
+//
+// Description: Driver class that does different kind of drives
+//
+
+public class el11DriveBot {
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/el11pkg/el11EventLoop.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.el11pkg;
+
+//
+// @Copyright : All rights reserved. Belongs to FTC Team 25610
+//
+
+//
+// Description: The main event loop. Tele-ops procedures are implemented are implemented here.
+//
+
+public class el11EventLoop {
+}
